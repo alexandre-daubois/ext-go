4881468e03d1056bdfaa07ff759064f9441cf6c7
Alexandre Daubois <alex.daubois@gmail.com>
2025-05-23 16:02:40 +0200

    Working...

--- ext-go.c
+++ ext-go.c
@@ -1,8 +1,3 @@
-#include <SAPI.h>
-#include <Zend/zend_alloc.h>
-#include <Zend/zend_exceptions.h>
-#include <Zend/zend_interfaces.h>
-#include <Zend/zend_types.h>
 #include <php.h>
 
 #include "_cgo_export.h"
