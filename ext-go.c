249650cdee11c658fc6f994d4b3e8193adbc4f0b
Alexandre Daubois <alex.daubois@gmail.com>
2025-05-23 16:56:45 +0200

    Working...

--- ext-go.c
+++ ext-go.c
@@ -1,6 +1,7 @@
 #include <php.h>
 
 #include "ext-go.h"
+#include "ext-go_arginfo.h"
 #include "_cgo_export.h"
 
 PHP_FUNCTION(go_print) /* {{{ */
--- ext-go.h
+++ ext-go.h
@@ -2,7 +2,6 @@
 #define _EXTGO_H
 
 #include <php.h>
-#include "ext-go_arginfo.h"
 
 #define PHP_EXT_GO_VERSION "0.1.0"
 #define PHP_EXT_GO_EXTNAME "ext_go"
