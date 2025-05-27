c11d4a5bbc049f92883ab33fa85c8596ac5596d3
Alexandre Daubois <alex.daubois@gmail.com>
2025-05-27 16:47:07 +0200

    Add `go_upper()`

--- ext-go_arginfo.h
+++ ext-go_arginfo.h
@@ -1,12 +1,22 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: df9f75614b1d1f9d33c12ea92f15f99d815dcd20 */
+ * Stub hash: 76b4b777ef5009ceb47f814d39b74fc4be91b92a */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_go_print, 0, 0, IS_VOID, 0)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_go_upper, 0, 1, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, string, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+
 ZEND_FUNCTION(go_print);
+ZEND_FUNCTION(go_upper);
+
 
 // clang-format off
 static const zend_function_entry ext_functions[] = {
-    ZEND_FE(go_print, arginfo_go_print) ZEND_FE_END};
+	ZEND_FE(go_print, arginfo_go_print)
+	ZEND_FE(go_upper, arginfo_go_upper)
+	ZEND_FE_END
+};
 // clang-format on
